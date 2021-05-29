7e40fb25a65cd7c24f051883fc04e00994ff0239
Kylox21 <71137493+Kylox21@users.noreply.github.com>
2021-05-29 17:52:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,93 @@
+
 const petsData = [
   {
-    name: 'The king of drugs',
-    bookname: 'Nora Barrett',
+    name: '1660ti',
     id: 1,
-    photo:
-      'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc_screen.jpg?ts=1588152105'
+    location : 'amd',
+    photo:'test.jpg'
   },
   {
-    name: 'Barksalot',
-    bookname: 'Dog',
-    href: 'book.html',
+    name: '120ti',
     id: 2,
+    location : 'amd',
     photo: 'https://learnwebcode.github.io/json-example/images/dog-1.jpg'
-  }
+  },
+  {
+    name: '1660ti',
+    id: 1,
+    location : 'amd',
+    photo:'test.jpg'
+  },
+  {
+    name: '1660ti',
+    id: 1,
+    location : 'amd',
+    photo:'test.jpg'
+  },
+  {
+    name: '1660ti',
+    id: 1,
+    location : 'amd',
+    photo:'test.jpg'
+  },
+  {
+    name: '1660ti',
+    id: 1,
+    location : 'amd',
+    photo:'test.jpg'
+  },
+  {
+    name: '1660ti',
+    id: 1,
+    location : 'amd',
+    photo:'test.jpg'
+  },
+  {
+    name: '1660ti',
+    id: 1,
+    location : 'amd',
+    photo:'test.jpg'
+  },
+  {
+    name: '1660ti',
+    id: 1,
+    location : 'amd',
+    photo:'test.jpg'
+  },
+  {
+    name: '1660ti',
+    id: 1,
+    location : 'amd',
+    photo:'test.jpg'
+  },
+  {
+    name: '1660ti',
+    id: 1,
+    location : 'amd',
+    photo:'test.jpg'
+  },
+  {
+    name: '1660ti',
+    id: 1,
+    location : 'amd',
+    photo:'test.jpg'
+  },
+  {
+    name: '1660ti',
+    id: 1,
+    location : 'amd',
+    photo:'test.jpg'
+  },
+  
 ];
 
 console.log('Name:', name);
 
 function petTemplate(pet, index) {
+
   return `
-<div class ="image-grid">
+  <div class = "shitter">
    <div class="animal">
-   
-    <img class="pet-photo " src="${pet.photo}" >
    
       
        <div class="olay" onclick="location.href='book.html?book=${index}';" style="cursor: pointer;">
@@ -31,12 +96,10 @@ function petTemplate(pet, index) {
       <h2 class="pet-name">${pet.name}
       
       
-      <h1 class="species">${pet.bookname}
 
-      </div>
-       <div></div></div>
-      </div>
-   </div>
+       </div></div>
+       </div>
+       </div>
 
   
 `;
@@ -44,7 +107,6 @@ function petTemplate(pet, index) {
 
 function indexPage() {
   document.getElementById('app').innerHTML = `
-   <h1 class="app-title">Kids</h1>
    ${petsData.map(petTemplate).join('')}
 `;
 }
@@ -57,9 +119,13 @@ function getTemplateFor() {
   let bookHtml = `
 <div class ="book-html">
 <h1>${book.name}  </h1>
-<p>${book.bookname}</p>
+<p>${book.location}</p>
 </div>
 `;
 
   document.getElementById('book_details').innerHTML = bookHtml;
 }
+
+
+//SEARCH FUNCTION
+
